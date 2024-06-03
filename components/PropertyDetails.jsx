b8b8d7875528b68710c218140bbdd7df91ae4835
amit <amit@rootfi.in>
2024-06-03 14:09:36 +0530

    small push

--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaBed, FaBath, FaRulerCombined, FaTimes, FaCheck, FaMapMarker } from "react-icons/fa";
+import PropertyMap from "./PropertyMap";
 
 const PropertyDetails = ({ property }) => {
   return (
@@ -60,7 +61,7 @@ const PropertyDetails = ({ property }) => {
         </ul>
       </div>
       <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-        <div id="map"></div>
+        <PropertyMap property={property} />
       </div>
     </main>
   );
---
+++ components/PropertyMap.jsx
@@ -0,0 +1,13 @@
+"use client";
+// import React, { useState, useEffect } from "react";
+// import "mapbox-gl/dist/mapbox-gl.css";
+// import Map, { Marker } from "react-map-gl";
+// import { setDefaults, fromAddress } from "react-geocode";
+// import Image from "next/image";
+// import pin from "@/assets/images/pin.png";
+
+const PropertyMap = () => {
+  return <div>PropertyMap</div>;
+};
+
+export default PropertyMap;
